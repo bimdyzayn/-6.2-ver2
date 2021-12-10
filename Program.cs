7d6afbab1b7c6739863b2a9f01c1692a45b2dded
dagremont@mail.ru <dagremont@mail.ru>
2021-12-11 01:24:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_6._2
+{
+   
+ 
+    class Palindr
+    {
+        public static bool Palindromtest(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                if (s[i] != s[j])
+
+                    return false;
+                
+            }
+            return true;
+        }
+
+        static void Main()
+        {
+            string s;
+            Console.WriteLine("Тест на палиндром\n\nВведите строку:");
+            s = Console.ReadLine();
+            s = s.Replace(" ", "");
+            if (Palindromtest(s) == true)
+                Console.WriteLine("Эта строка - палиндром");
+            else Console.WriteLine("Эта строка - не палиндром");
+            Console.ReadLine();
+        }
+    }
+}
+  
+
